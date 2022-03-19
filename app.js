b771f2cd8e485a002fd1ef4a1199b3688e0aefb2
zishan <zishxn@gmail.com>
2022-03-19 23:04:29 +0530

    Remove traingles and add fontawesome

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const starsArray = ['star-1', 'star-2', 'star-3', 'star-4', 'star-5'];
 const onHover = (star) => {
     star.addEventListener("mouseenter", (event) => {
         const targetId = event.target.id;
-        console.log("target id 1", targetId);
         for (let i = 0; i <= starsArray.indexOf(targetId); i++) {
-            [...document.getElementById(starsArray[i]).children][0].style.borderBottomColor = "#FFE99D";
-            [...document.getElementById(starsArray[i]).children][1].style.borderTopColor = "#FFE99D";
+            document.getElementById(starsArray[i]).style.color = "#FFE99D";
         }
     })
     star.addEventListener("mouseleave", (event) => {
-        const targetId = event.target.id;
-        [...document.getElementById(targetId).children][0].style.borderBottomColor = "#f1f1f1";
-        [...document.getElementById(targetId).children][1].style.borderTopColor = "#f1f1f1";
+        for (let i = 0; i < starsArray.length; i++) {
+            document.getElementById(starsArray[i]).style.color = "#f1f1f1";
+        }
 
     })
 
@@ -22,10 +20,8 @@ const onHover = (star) => {
 const onClick = (star) => {
     star.addEventListener("click", (event) => {
         const targetId = event.target.id;
-        console.log("target id 2", targetId);
         for (let i = 0; i <= starsArray.indexOf(targetId); i++) {
-            [...document.getElementById(starsArray[i]).children][0].style.borderBottomColor = "#FED41D";
-            [...document.getElementById(starsArray[i]).children][1].style.borderTopColor = "#FED41D";
+            document.getElementById(starsArray[i]).style.color = "#FED41D";
         }
     })
 
